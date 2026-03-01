7a5d5b5bc1495ae8a6f32d1e4c809a6b74bcb45f
Yaseem <yaseem@hcltech.com>
2026-03-02 02:04:43 +0530

    Use standard Azure environment variables

--- provider.tf
+++ provider.tf
@@ -16,9 +16,7 @@ terraform {
 }   
     
 provider "azurerm" {
-    subscription_id = var.azure_subscription_id
-    tenant_id       = var.azure_tenant_id
-    client_id       = var.azure_client_id
-    client_secret   = var.azure_client_secret
+    # Azure credentials are supplied via environment variables:
+    # ARM_SUBSCRIPTION_ID, ARM_TENANT_ID, ARM_CLIENT_ID, ARM_CLIENT_SECRET
     features {}
 }
--- variables.tf
+++ variables.tf
@@ -1,27 +1,3 @@
-variable "azure_subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-  sensitive   = false
-}
-
-variable "azure_tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-  sensitive   = false
-}
-
-variable "azure_client_id" {
-  description = "Azure client ID (service principal app ID)"
-  type        = string
-  sensitive   = false
-}
-
-variable "azure_client_secret" {
-  description = "Azure client secret (service principal password)"
-  type        = string
-  sensitive   = true
-}
-
 variable "enviroment" {
     description = "The environment for the resources"
     type        = string
